2aec8d9cad9a05c7b7bf06f8d0f21f71f04e4425
iamdiniz <guilhermeplays03@gmail.com>
2025-06-18 11:00:52 -0300

    criando testes felizes e infelizes e criando uma factorie

---
+++ tests/schemas/factories.py
@@ -0,0 +1,3 @@
+def product_data():
+    data = {"name": "Xbox", "quantity": 10, "price": 2.000, "status": True}
+    return data
--- tests/schemas/test_product.py
+++ tests/schemas/test_product.py
@@ -1,10 +1,30 @@
+from pydantic import ValidationError
+import pytest
 from uuid import UUID
 from store.schemas.product import ProductIn
+from tests.schemas.factories import product_data
 
 
-def test_schemas_validated():
-    data = {"name": "Xbox", "quantity": 10, "price": 2.000, "status": True}
-    product = ProductIn(**data)
+def test_schemas_return_sucess():
+    data = product_data()
+    product = ProductIn.model_validate(
+        data
+    )  # pode passar usando o model validate tambem
 
     assert product.name == "Xbox"
     assert isinstance(product.id, UUID)
+
+
+def test_schemas_return_raise():
+    data = {"name": "Xbox", "quantity": 10, "price": 2.000}
+
+    with pytest.raises(ValidationError) as e:
+        ProductIn.model_validate(data)
+
+    assert e.value.errors()[0] == {
+        "type": "missing",
+        "loc": ("status",),
+        "msg": "Field required",
+        "input": {"name": "Xbox", "quantity": 10, "price": 2.0},
+        "url": "https://errors.pydantic.dev/2.11/v/missing",
+    }
