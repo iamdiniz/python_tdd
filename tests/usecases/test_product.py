ea2b667638b43d80e5fab8887cf78ae1c97d2c05
iamdiniz <guilhermeplays03@gmail.com>
2025-06-18 17:28:02 -0300

    criando mais testes de usecases

--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -1,8 +1,21 @@
+from uuid import UUID
+from store.schemas.product import ProductOut
 from store.usecases.product import product_usecase
 
 
 async def test_usecases_should_return_sucess(product_in):
     result = await product_usecase.create(body=product_in)
 
-    # assert isinstance(result, ProductOut)
-    assert result is None
+    assert isinstance(result, ProductOut)
+    assert result.name == "Xbox"
+
+
+async def test_usecases_get_should_return_sucess(product_id):
+    result = await product_usecase.get(id=product_id)
+
+    assert isinstance(result, ProductOut)
+    assert result.name == "Xbox"
+
+
+async def test_usecases_get_should_not_found():
+    await product_usecase.get(id=UUID("64e6b7c46a4e4f1a8d2d3b9c"))
